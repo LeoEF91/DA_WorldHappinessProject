70d17c6404d452b2d4a207e9584847477fae8b58
kiszalb <166434233+kiszalb@users.noreply.github.com>
2024-07-21 23:20:28 +0200

    Update Test20Jul24.py

--- Test20Jul24.py
+++ Test20Jul24.py
@@ -40,7 +40,7 @@ st.sidebar.markdown("""
     </a></p>
     <p style='font-family:Calibri (Body); color:blue;'><strong>2. Marta Blazsik</strong> 
    <a href="https://www.linkedin.com/in/marta.blazsik">
-    <img src="https://www.example.com/path/to/linkedin-logo.png" alt="LinkedIn Logo">
+    <img src="https://www.flaticon.com/free-icon/linkedin_174857">
 	</a>
     <p style='font-family:Calibri (Body); color:blue;'><strong>3. Nitin Zimur</strong> 
     <a href='https://www.linkedin.com/in/nitin-z-9a662019/' style='color:blue;'>
