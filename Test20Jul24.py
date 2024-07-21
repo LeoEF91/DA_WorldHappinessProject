ad2181f2d64c6820c2dd3050e5eb07d258d55a89
kiszalb <166434233+kiszalb@users.noreply.github.com>
2024-07-21 23:11:04 +0200

    Update Test20Jul24.py

--- Test20Jul24.py
+++ Test20Jul24.py
@@ -35,17 +35,13 @@ page = st.sidebar.radio("Go to", pages)
 st.sidebar.markdown("""
     <h2 style='font-family:Calibri (Body); color:Orange;'>Project Team Members</h2>
     <p style='font-family:Calibri (Body); color:blue;'><strong>1. Leonie E. Fickinger</strong> 
-    <a href='https://www.linkedin.com/in/leonie-ederli-fickinger-424a24117/' style='color:blue;'>
-    <img src='https://www.flaticon.com/free-icon/linkedin_174857'>
-    </a></p>
+    "![](https://www.pixenli.com/image/fm0aEpMI)"
     <p style='font-family:Calibri (Body); color:blue;'><strong>2. Marta Blazsik</strong> 
     <a href='https://www.linkedin.com/in/martablazsik' style='color:blue;'>
-    <img src='https://www.flaticon.com/free-icon/linkedin_174857'>
-    </a></p>
+   "![](https://www.pixenli.com/image/fm0aEpMI)"
     <p style='font-family:Calibri (Body); color:blue;'><strong>3. Nitin Zimur</strong> 
     <a href='https://www.linkedin.com/in/nitin-z-9a662019/' style='color:blue;'>
-    <img src='https://www.flaticon.com/free-icon/linkedin_174857'>
-    </a></p>
+   "![](https://www.pixenli.com/image/fm0aEpMI)"
     """, unsafe_allow_html=True)
 
 # Load the datasets
