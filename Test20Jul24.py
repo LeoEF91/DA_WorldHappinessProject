b647c6aeb5728be67ebf116bf6baa605b1ef0ff4
kiszalb <166434233+kiszalb@users.noreply.github.com>
2024-07-21 22:56:52 +0200

    Update Test20Jul24.py

--- Test20Jul24.py
+++ Test20Jul24.py
@@ -36,15 +36,15 @@ st.sidebar.markdown("""
     <h2 style='font-family:Calibri (Body); color:Orange;'>Project Team Members</h2>
     <p style='font-family:Calibri (Body); color:blue;'><strong>1. Leonie E. Fickinger</strong> 
     <a href='https://www.linkedin.com/in/leonie-ederli-fickinger-424a24117/' style='color:blue;'>
-    <img src='https://upload.wikimedia.org/wikipedia/commons/0/01/LinkedIn_Logo.svg' alt='LinkedIn' width='30' height='20'>
+    <img src='LI-In-Bug.png' alt='LinkedIn' width='30' height='20'>
     </a></p>
     <p style='font-family:Calibri (Body); color:blue;'><strong>2. Marta Blazsik</strong> 
     <a href='https://www.linkedin.com/in/martablazsik' style='color:blue;'>
-    <img src='https://upload.wikimedia.org/wikipedia/commons/0/01/LinkedIn_Logo.svg' alt='LinkedIn' width='30' height='20'>
+    <img src='LI-In-Bug.png' alt='LinkedIn' width='30' height='20'>
     </a></p>
     <p style='font-family:Calibri (Body); color:blue;'><strong>3. Nitin Zimur</strong> 
     <a href='https://www.linkedin.com/in/nitin-z-9a662019/' style='color:blue;'>
-    <img src='https://upload.wikimedia.org/wikipedia/commons/0/01/LinkedIn_Logo.svg' alt='LinkedIn' width='30' height='20'>
+    <img src='LI-In-Bug.png' alt='LinkedIn' width='30' height='20'>
     </a></p>
     """, unsafe_allow_html=True)
 
