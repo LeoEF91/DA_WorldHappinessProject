7a3eee2bcde049670651e24d2d31761da448fa48
LeoEF91 <l.ederli.fickinger@gmail.com>
2024-07-22 18:41:23 +0200

    Update Test20Jul24.py

--- Test20Jul24.py
+++ Test20Jul24.py
@@ -104,7 +104,7 @@ if page == pages[0]:
 if page == pages[1]:
 
 
-    st.write("### Relevance: Target and Feature Variables")
+    st.markdown("<h1 style='text-align: left;'>Relevance: Target and Feature Variables</h1>", unsafe_allow_html=True)
     
     st.write("""
     To predict the happiness scores of countries, six feature variables were selected.
