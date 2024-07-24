bd5a95230e9d3f7975f1f90403fd2796e129b36c
kiszalb <166434233+kiszalb@users.noreply.github.com>
2024-07-24 15:02:14 +0200

    Update Test20Jul24.py

--- Test20Jul24.py
+++ Test20Jul24.py
@@ -34,15 +34,15 @@ page = st.sidebar.radio("Go to", pages)
 
 st.sidebar.markdown("""
     <h2 style='font-family:Calibri (Body); color:Orange;'>Project Team Members</h2>
-    <p style='font-family:Calibri (Body); color:blue;'><strong>1. Leonie E. Fickinger</strong> 
+    <p style='font-family:Calibri (Body); color:blue;'><strong>Leonie E. Fickinger</strong> 
     <a href='https://www.linkedin.com/in/leonie-ederli-fickinger-424a24117/' style='color:blue;'>
     <img src='https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg'  width='20' height='20'>
     </a></p>
-    <p style='font-family:Calibri (Body); color:blue;'><strong>2. Marta Blazsik</strong> 
+    <p style='font-family:Calibri (Body); color:blue;'><strong>Marta Blazsik</strong> 
     <a href='https://www.linkedin.com/in/martablazsik' style='color:blue;'>
     <img src='https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg'  width='20' height='20'>
     </a></p>
-    <p style='font-family:Calibri (Body); color:blue;'><strong>3. Nitin Zimur</strong> 
+    <p style='font-family:Calibri (Body); color:blue;'><strong>Nitin Zimur</strong> 
     <a href='https://www.linkedin.com/in/nitin-z-9a662019/' style='color:blue;'>
     <img src='https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg'  width='20' height='20'>
     </a></p>
